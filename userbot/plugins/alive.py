8e7582478ce2c5670f5da5aa686a098d184cce91
THUG <73270173+THUGOFFICIALS@users.noreply.github.com>
2020-10-23 16:32:53 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,7 +5,7 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
 PM_IMG = "https://telegra.ph/file/f34675b4e94d4290c0b6b.mp4"
-pm_caption = "🔥🔥**Hêllẞø† IS ONLINE🔥🔥\n\n\n"
+pm_caption = "👌**𝗧𝗛𝗨𝗚𝗕𝗢𝗧 IS ONLINE Dude 👌**\n\n\n"
 
 pm_caption += f"**MASTER**       : {DEFAULTUSER}\n\n"
 
@@ -15,7 +15,7 @@ pm_caption += "⚠️⚠️**CHANNEL**⚠️⚠️       : [JOIN](https://t.me/T
 
 pm_caption += "🏢🏢**SUPPORT**🏢🏢.       : [JOIN](https://t.me/THUG_SUPPORT)\n\n"
 
-pm_caption += "👼👼GOD👼👼              : [THUG](https://t.me/brokendeadskull)\n\n"
+pm_caption += "👼👼**GOD**👼👼              : [THUG](https://t.me/brokendeadskull)\n\n"
 
 pm_caption += "[┈┈┈┈┈┈▕▔╲┈┈┈┈┈┈\n┈┈┈┈┈┈┈▏▕┈THUGS\n┈┈┈┈┈┈┈▏▕▂▂▂┈┈┈\n▂▂▂▂▂▂╱┈▕▂▂▂▏┈┈\n▉▉▉▉▉┈┈┈▕▂▂▂▏┈┈\n▉▉▉▉▉┈┈┈▕▂▂▂▏┈┈\n▔▔▔▔▔▔╲▂▕▂▂▂▏┈┈\n](https://t.me/thugs_support)\n\n"
 #@command(outgoing=True, pattern="^.alive$")
